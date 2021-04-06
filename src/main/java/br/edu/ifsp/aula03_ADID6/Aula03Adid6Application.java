45cd87651d9e5238de941d96f2eda8259f82d8f5
svntxz <viniciussantos1314@gmail.com>
2021-04-06 20:37:39 -0300

    spring initializer

---
+++ src/main/java/br/edu/ifsp/aula03_ADID6/Aula03Adid6Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula03_ADID6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula03Adid6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula03Adid6Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/aula03_ADID6/Aula03Adid6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula03_ADID6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula03Adid6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
